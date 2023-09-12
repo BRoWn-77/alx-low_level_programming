4e45fb74de8d5fc284e6d1b6663af9f559af3746
BRoWn-77 <mosesbinkat@gmail.com>
2023-09-12 07:03:27 -0700

    print_alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Entry point
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0
  */
 int print_alphabet_x10(void)
 {
