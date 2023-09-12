c45a6050e4d07d28349a1529faf297035a6f33ab
BRoWn-77 <mosesbinkat@gmail.com>
2023-09-12 14:03:59 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@
  * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
  * followed by a new line
  *
- * Return: Always 0
+ * Return: void
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
