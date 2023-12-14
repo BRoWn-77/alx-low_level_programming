e5f5916e83084dfae7dd0e52debb577613e467dd
BRoWn-77 <mosesbinkat@gmail.com>
2023-12-14 05:58:34 -0800

    all

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * div - division of two integers
+ * @a: first int
+ * @b: second int
+ * Return: answer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - mul of two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: first int
+ * @b: second int
+ * Return: answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final results
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * mul - mul of two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int add(int a, int b)
+{
+	return (a * b);
+}
